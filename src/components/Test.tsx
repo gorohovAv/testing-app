90f8cd6e731023572e189f3b92e49fa8ae6d3a6b
gorohovAv <gorohov.av@edu.spbstu.ru>
2024-10-03 20:59:51 +0300

    mock

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -2,6 +2,8 @@ import Question from "./Question";
 import ProgressBar from "./ProgressBar";
 import Timer from "./Timer";
 import {Step} from "../types/progress";
+import type {Test} from "../types/test"
+import {Selection, TextInput, SelectionType, InputType} from "../types/question";
 
 const steps: Step[] = [
   { isDone: false, isActive: true },
@@ -16,7 +18,64 @@ const steps: Step[] = [
   { isDone: true, isActive: false },
 ];
 
-const Test = () => {
+const myTest: Test<Selection | TextInput> = {
+  header: "Тест по общим знаниям",
+  questions: [
+    {
+      header: "Что такое HTML?",
+      variants: ["Язык программирования", "Язык разметки", "Система управления базами данных"],
+      type: SelectionType.Unary,
+    },
+    {
+      header: "Выберите правильные утверждения о CSS:",
+      variants: ["Он служит для стилизации веб-страниц", "Он не может использоваться с HTML", "Может быть встроен в HTML"],
+      type: SelectionType.Multiple,
+    },
+    {
+      header: "Какой язык используется для серверного программирования?",
+      variants: ["JavaScript", "HTML", "CSS", "Python"],
+      type: SelectionType.Unary,
+    },
+    {
+      header: "Какой элемент HTML используется для создания ссылок?",
+      variants: ["<link>", "<a>", "<href>", "<url>"],
+      type: SelectionType.Unary,
+    },
+    {
+      header: "Опишите, что такое JavaScript:",
+      placeholder: "Ваш ответ здесь...",
+      type: InputType.Short,
+    },
+    {
+      header: "Какой метод используется для добавления элемента в массив в JavaScript?",
+      variants: ["add()", "append()", "push()", "insert()"],
+      type: SelectionType.Unary,
+    },
+    {
+      header: "Выберите преимущества использования фреймов:",
+      variants: ["Упрощает код", "Повышает производительность", "Снижает связность", "Упрощает стилизацию"],
+      type: SelectionType.Multiple,
+    },
+    {
+      header: "Напишите основное отличие между <div> и <span>:",
+      placeholder: "Ваш ответ здесь...",
+      type: InputType.Long,
+    },
+    {
+      header: "Что такое AJAX?",
+      variants: ["Язык программирования", "Метод работы с базами данных", "Технология для создания асинхронных запросов", "Стандарт для веб-дизайна"],
+      type: SelectionType.Unary,
+    },
+    {
+      header: "Опишите, что такое Responsive Web Design:",
+      placeholder: "Ваш ответ здесь...",
+      type: InputType.Short,
+    },
+  ],
+  time: {hours: 0, minutes: 30, seconds: 0}
+};
+
+const Test: React.FC<Test> = () => {
 
   return (
     <>
